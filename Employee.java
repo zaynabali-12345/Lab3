462ba7e05ad5d8aadc77dbcc7dd6b197082c9b72
zaynabali-12345 <zaynabali00786@gmail.com>
2025-05-06 14:27:51 +0530

    message

--- Employee.java
+++ Employee.java
@@ -2,10 +2,12 @@ public class Employee {
     String name;
     int id;
     double salary;
-    public Employee(String name, int id, double salary){
+    String dob;
+    public Employee(String name, int id, double salary,String dob){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.dob=dob;
     }
     public void displayInfo(){
         System.out.println("Employee ID:"+id);
@@ -13,7 +15,7 @@ public class Employee {
         System.out.println("Salary: "+salary);
     }
     public static void main(String[] args) {
-        Employee e1 = new Employee("XYZ", 101, 50000.0);
+        Employee e1 = new Employee("XYZ", 101, 50000.0,"26-11-2005");
         e1.displayInfo();
     }
 }
